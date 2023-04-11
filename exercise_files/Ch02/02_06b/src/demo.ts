8c1874ba77811a1caaec09e7d098a3838bb61878
Daniel Orlov <daniel.orlov@protonmail.com>
2023-04-11 19:25:24 +0200

    feat: generic practice
    Signed-off-by: Daniel Orlov <daniel.orlov@protonmail.com>

--- exercise_files/Ch02/02_06b/src/demo.ts
+++ exercise_files/Ch02/02_06b/src/demo.ts
@@ -3,9 +3,36 @@ interface Contact {
     name: string;
 }
 
-function clone(source: Contact): Contact {
-    return Object.apply({}, source);
+interface UserContact extends Contact {
+    email: string;
 }
 
-const a: Contact = { id: 123, name: "Homer Simpson" };
-const b = clone(a)
+interface EnjoyerContact {
+    id: number;
+    name: string;
+    email: string;
+    role: string;
+}
+
+function clone<T1, T2 extends T1>(source: T1): T2 {
+    const serialized = JSON.stringify(source);
+    return JSON.parse(serialized);
+}
+
+const homer: Contact = {id: 123, name: "Homer Simpson"};
+const homerClone = clone<Contact, EnjoyerContact>(homer);
+
+console.log(homerClone.id, homerClone.name);
+
+const secondClone = clone<Contact, UserContact>(homer);
+
+console.log(secondClone.id, secondClone.name);
+
+const dateRange = {
+    from: new Date(2018, 1, 1),
+    to: new Date(2018, 1, 31)
+};
+
+const clonedDateRange = clone(dateRange);
+
+console.log(clonedDateRange.from, clonedDateRange.to);
