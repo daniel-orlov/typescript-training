8947c43c7d5867861db9545e35406288b5b22632
Daniel Orlov <daniel.orlov@protonmail.com>
2023-04-11 18:57:59 +0200

    feat: interfaces practice

--- exercise_files/Ch02/02_02e/src/demo.ts
+++ exercise_files/Ch02/02_02e/src/demo.ts
@@ -1,7 +1,7 @@
-interface Contact extends Address {
+interface Contact extends Address { // Extends Address interface, so it has all the properties of Address.
     id: number;
     name: string;
-    birthDate?: Date;
+    birthDate?: Date; // Optional property
 }
 
 interface Address {
@@ -16,5 +16,9 @@ let primaryContact: Contact = {
     birthDate: new Date("01-01-1980"),
     id: 12345,
     name: "Jamie Johnson",
-    postalCode: 
-}
+    postalCode: "V1V 1V1",
+    line1: "123 Main Street",
+    line2: "Suite 100",
+    province: "BC",
+    region: "Vancouver"
+};
