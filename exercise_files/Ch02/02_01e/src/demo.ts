bc101e6bd487a8aa3a46e28f5945e64c8b4db3d9
Daniel Orlov <daniel.orlov@protonmail.com>
2023-04-11 18:52:48 +0200

    feat: ts type practice

--- exercise_files/Ch02/02_01e/src/demo.ts
+++ exercise_files/Ch02/02_01e/src/demo.ts
@@ -1,8 +1,14 @@
-let x: number
-let y: string
-let z: boolean
-let a: Date
-let b: string[]
+let x: number;
+let y: string;
+let z: boolean;
+let a: Date;
+let b: string[];
 
-b = "Hello!" as any
-b = 1234
+b = "Hello!" as any;
+b = 1234 as any;
+
+x = 1234;
+y = "Hello!";
+z = true;
+a = new Date();
+b = ["Hello", "World"];
