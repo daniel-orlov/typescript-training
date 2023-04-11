850cfa60c0ceb1cc63d4fc4659ed7fdcc21f663a
Daniel Orlov <daniel.orlov@protonmail.com>
2023-04-11 19:16:41 +0200

    feat: method and function practice

--- exercise_files/Ch02/02_05b/src/demo.ts
+++ exercise_files/Ch02/02_05b/src/demo.ts
@@ -1,8 +1,22 @@
 interface Contact {
     id: number;
     name: string;
+
+    // Methods
+    clone(): Contact;
 }
 
-function clone(source) {
-    return Object.apply({}, source);
+function clone(source: Contact): Contact {
+    return {...source};
 }
+
+const contact: Contact = {
+    id: 1,
+    name: "Valkyrie",
+    clone(): Contact {
+        return clone(this);
+    }
+};
+
+const clonedContact = contact.clone();
+console.log(clonedContact);
