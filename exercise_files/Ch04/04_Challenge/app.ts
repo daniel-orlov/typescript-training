0c6fda8167cf8cfb63494efee8c9f3c2546a99fa
Daniel Orlov <daniel.orlov@protonmail.com>
2023-04-16 12:36:54 +0200

    feat: solve Challenge03
    Signed-off-by: Daniel Orlov <daniel.orlov@protonmail.com>

--- exercise_files/Ch04/04_Challenge/app.ts
+++ exercise_files/Ch04/04_Challenge/app.ts
@@ -1,31 +1,33 @@
 function query<T>(
     items: T[],
-    query: any // <--- replace this!
+    query: {
+        [TProp in keyof T]?: (val: T[TProp]) => boolean
+    }
 ) {
     return items.filter(item => {
         // iterate through each of the item's properties
         for (const property of Object.keys(item)) {
 
             // get the query for this property name
-            const propertyQuery = query[property]
+            const propertyQuery = query[property];
 
             // see if this property value matches the query
             if (propertyQuery && propertyQuery(item[property])) {
-                return true
+                return true;
             }
         }
 
         // nothing matched so return false
-        return false
-    })
+        return false;
+    });
 }
 
 const matches = query(
     [
-        { name: "Ted", age: 12 },
-        { name: "Angie", age: 31 }
+        {name: "Ted", age: 12},
+        {name: "Angie", age: 31}
     ],
     {
         name: name => name === "Angie",
         age: age => age > 30
-    })
+    });
