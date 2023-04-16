37c2617464e39fce12587188fdef1b425514be6d
Daniel Orlov <daniel.orlov@protonmail.com>
2023-04-16 12:26:07 +0200

    feat: map type practice
    Signed-off-by: Daniel Orlov <daniel.orlov@protonmail.com>

--- exercise_files/Ch04/04_02b/src/demo.ts
+++ exercise_files/Ch04/04_02b/src/demo.ts
@@ -14,18 +14,24 @@ interface Contact {
     email: string;
 }
 
-interface Query {
-    sort?: 'asc' | 'desc';
-    matches(val): boolean;
+interface Query<TProp> {
+    sort?: "asc" | "desc";
+
+    matches(val: TProp): boolean;
 }
 
-type ContactQuery = Partial<Record<keyof Contact, Query>>;
+// type ContactQuery = Partial<Record<keyof Contact, Query>>;
+
+// Using Map
+type ContactQuery = {
+    [TProp in keyof Contact]?: Query<Contact[TProp]>
+}
 
 function searchContacts(contacts: Contact[], query: ContactQuery) {
     return contacts.filter(contact => {
         for (const property of Object.keys(contact) as (keyof Contact)[]) {
             // get the query object for this property
-            const propertyQuery = query[property];
+            const propertyQuery = query[property] as Query<Contact[keyof Contact]>;
             // check to see if it matches
             if (propertyQuery && propertyQuery.matches(contact[property])) {
                 return true;
@@ -33,13 +39,13 @@ function searchContacts(contacts: Contact[], query: ContactQuery) {
         }
 
         return false;
-    })
+    });
 }
 
 const filteredContacts = searchContacts(
     [/* contacts */],
     {
-        id: { matches: (id) => id === 123 },
-        name: { matches: (name) => name === "Carol Weaver" },
+        id: {matches: (id) => id === 123},
+        name: {matches: (name) => name === "Carol Weaver"},
     }
 );
