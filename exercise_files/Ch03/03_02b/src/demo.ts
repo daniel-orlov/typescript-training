341b5f9707e768f6ac49a9c91b978feea4e72f33
Daniel Orlov <daniel.orlov@protonmail.com>
2023-04-16 11:35:03 +0200

    feat: keyof operator practice
    Signed-off-by: Daniel Orlov <daniel.orlov@protonmail.com>

--- exercise_files/Ch03/03_02b/src/demo.ts
+++ exercise_files/Ch03/03_02b/src/demo.ts
@@ -2,7 +2,7 @@ type ContactName = string;
 type ContactStatus = "active" | "inactive" | "new"
 type ContactBirthDate = Date | number | string
 
-interface Contact  {
+interface Contact {
     id: number;
     name: ContactName;
     birthDate?: ContactBirthDate;
@@ -13,4 +13,18 @@ let primaryContact: Contact = {
     id: 12345,
     name: "Jamie Johnson",
     status: "active"
+};
+
+type ContactFields = keyof Contact
+
+const field: ContactFields = "status";
+
+function getValue<T>(source: T, propertyName: keyof T) {
+    return source[propertyName];
 }
+
+const value = getValue(primaryContact, "status");
+
+const anotherValue = getValue({key: 2, otherKey: 3}, "key");
+
+
